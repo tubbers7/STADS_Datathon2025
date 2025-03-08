c8d121204a0586b6d43c80cba16318eaabcfedbf
tubbers7 <toby.fuchs@me.com>
2025-03-08 12:55:03 +0100

    exploration table

--- pages/2_Explorative_Analysis.py
+++ pages/2_Explorative_Analysis.py
@@ -4,14 +4,15 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 from untitled import load_influenza_df, load_age_df, load_vaccine_df, merge_vac_cases, create_geo_df
+from tables_for_exploration import load_df_p1, load_df_p2,load_df_p3,load_df_p4
 import plotly.express as px
 
 # --- Load Data ---
-df = pd.read_csv("C:\\Users\\Arved\\Desktop\\STADS_Datathon2025\\20025-03-07_cgm-datathon-challenge-flu_riskgroupsv1_cleaned.csv", sep=";")
+df = pd.read_csv("/Users/toby/STADS_Datathon2025/final_merged_no_dupe.csv", sep=",")
 
 # --- Filter Data ---
-df_gender = df[(df["gender"] == 'm') | (df["gender"] == 'f')]
-df_rg = df[df['risk_groups'].notnull()]
+#df_gender = df[(df["gender"] == 'm') | (df["gender"] == 'f')]
+#df_rg = df[df['risk_groups'].notnull()]
 
 
 # --- Streamlit UI ---
@@ -25,17 +26,17 @@ st.write(df.head())
 st.subheader("Extrapolated Data by Categories")
 
 st.write("Plot 1 - Gender and Insurance Type")
-st.bar_chart(df_gender, x="gender", y="extrapolated", color="insurancetype", stack=False,
-             y_label = "Gender", x_label = "Extrapolated Amount of Vaccinations", horizontal=True)
+st.bar_chart(load_df_p1(df), x="gender", y="Ratio", color="insurancetype", stack=False,
+             y_label = "Gender", x_label = "Extrapolated Vaccination Percentage", horizontal=True)
 
 st.write("Plot 2 - Age Group and Insurance Type")
-st.bar_chart(df, x="age_group", y="extrapolated", color="insurancetype", stack=False,
-                y_label = "Age Group", x_label = "Extrapolated Amount of Vaccinations", horizontal=True)
+st.bar_chart(load_df_p2(df), x="age_group", y="Ratio", color="insurancetype", stack=False,
+                y_label = "Age Group", x_label = "Extrapolated Vaccination Percentage", horizontal=True)
 
 st.write("Plot 3 - Risk Groups and Age Group")
-st.bar_chart(df_rg, x="risk_groups", y="extrapolated", color="age_group", stack=False,
-                y_label = "Risk Groups", x_label = "Extrapolated Amount of Vaccinations", horizontal=True)
+st.bar_chart(load_df_p3(df), x="risk_groups", y="Ratio", color="age_group", stack=False,
+                y_label = "High Risk Individuals", x_label = "Extrapolated Vaccination Percentage", horizontal=True)
 
 st.write("Plot 4 - Federal State and Insurance Type")
-st.bar_chart(df, x="kvregion", y="extrapolated", color="age_group", stack=False,
-                y_label = "Federal State", x_label = "Extrapolated Amount of Vaccinations", horizontal=True)
+st.bar_chart(load_df_p4(df), x="kvregion", y="Ratio", color="age_group", stack=False,
+                y_label = "Federal State", x_label = "Extrapolated Vaccination Percentage", horizontal=True)
