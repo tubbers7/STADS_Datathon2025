a17242d06f68458f4648923d97ec4c779647d5cb
arschrei <arved.schreiber@gmx.de>
2025-03-08 13:35:49 +0100

    ea

--- pages/2_Explorative_Analysis.py
+++ pages/2_Explorative_Analysis.py
@@ -7,7 +7,9 @@ from untitled import load_influenza_df, load_age_df, load_vaccine_df, merge_vac_
 import plotly.express as px
 
 # --- Load Data ---
-df = pd.read_csv("C:\\Users\\Arved\\Desktop\\STADS_Datathon2025\\20025-03-07_cgm-datathon-challenge-flu_riskgroupsv1_cleaned.csv", sep=";")
+df = pd.read_csv("C:\\Users\\Arved\\Desktop\\STADS_Datathon2025\\final_merged_no_dupe.csv", sep=",")
+# drop first column
+df = df.drop(df.columns[0], axis=1)
 
 # --- Filter Data ---
 df_gender = df[(df["gender"] == 'm') | (df["gender"] == 'f')]
@@ -36,6 +38,6 @@ st.write("Plot 3 - Risk Groups and Age Group")
 st.bar_chart(df_rg, x="risk_groups", y="extrapolated", color="age_group", stack=False,
                 y_label = "Risk Groups", x_label = "Extrapolated Amount of Vaccinations", horizontal=True)
 
-st.write("Plot 4 - Federal State and Insurance Type")
+st.write("Plot 4 - Federal State and Age Group")
 st.bar_chart(df, x="kvregion", y="extrapolated", color="age_group", stack=False,
                 y_label = "Federal State", x_label = "Extrapolated Amount of Vaccinations", horizontal=True)
